b60526c604eb7578dc048a5a7a47a7eeb2ef879e
Nikki A <nikki.apostolakis@gmail.com>
2018-09-01 23:54:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is ____"
+  end
+
+  get '/hometown' do
+    "My hometown is ____"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is ____"
+  end
+
 end
